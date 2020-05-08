d176f61e4edd6dc53e6bea35eb2fca30a44a2e37
AytakinAghayeva1176 <aytakinia@code.edu.az>
2020-05-09 02:16:58 +0400

    hw

---
+++ csharp-classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv = new Invoice("qwerty", "aytin", "qwer")
+            {
+                article = "laptop",
+                quantity = "1"
+            };
+
+            inv.CostCalculation(true);
+        }
+
+    }
+}
